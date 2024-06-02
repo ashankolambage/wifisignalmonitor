3472cda350fd1fa9ca4eb3120bf3bc2cabbb4b87
Ashan Kolambage <ashan@thesanmark.com>
2024-06-02 16:21:51 +0530

    ..

--- app/app/Http/Controllers/ApiController.php
+++ app/app/Http/Controllers/ApiController.php
@@ -11,15 +11,9 @@ class ApiController extends Controller
     public function getSignalData()
     {
         $url = 'http://192.168.8.1/cgi-bin/lua.cgi';
-        $url2 = 'http://192.168.8.1/reqproc/proc_get?isTest=false&cmd=system_status&_=1717323398508';
+        $url2 = 'http://192.168.8.1/reqproc/proc_get?isTest=false&cmd=system_status';
 
-        $routeType = "huawei";
-
-        if ($routeType = "huawei") {
-            # code...
-        }elseif ($routeType = "outdoor") {
-            # code...
-        }
+        $routerType = "outdoor";
 
         $postData = [
             'cmd' => 250,
@@ -28,17 +22,23 @@ class ApiController extends Controller
         ];
 
         try {
-            if ($routeType = "huawei") {
+            if ($routerType == "huawei") {
+                Log::info(__LINE__);
                 $response = Http::post($url, $postData);
-            }elseif ($routeType = "outdoor") {
-                $response = Http::post($url2);
+            }elseif ($routerType == "outdoor") {
+                Log::info(__LINE__);
+                $response = Http::get('http://192.168.8.1/reqproc/proc_get', [
+                    'isTest' => 'false',
+                    'cmd' => 'system_status'
+                ]);
             }
             
 
             if ($response->successful()) {
                 $data = $response->json();
 
-                if ($routeType = "huawei") {
+                if ($routerType == "huawei") {
+                    Log::info(__LINE__);
                     $filteredData = [
                         'modem_rsrp' => 150 + $data['data']['main_info']['modem_rsrp'] ?? 0,
                         'modem_rssi' => 150 + $data['data']['main_info']['modem_rssi'] ?? 0,
@@ -57,14 +57,29 @@ class ApiController extends Controller
                         'modem_rsrq' => $data['data']['main_info']['modem_rsrq'] ?? 0,
                         'modem_sinr' => $data['data']['main_info']['modem_sinr'] ?? 0,
                     ];
-                }elseif ($routeType = "outdoor") {
-                    Log::info(json_encode($response));
+                }elseif ($routerType == "outdoor") {
+                    $data = json_decode($response, true);
+
+
+                    $rsrq = $data['rsrq'];
+                    $rsrp = $data['rsrp'];
+                    $rssi = $data['rssi'];
+                    $sinr = $data['sinr'];
+
+                    Log::info("RSRQ: $rsrq");
+                    Log::info("RSRP: $rsrp");
+                    Log::info("RSSI: $rssi");
+                    Log::info("SINR: $sinr");
+                    
+                    Log::info("######");
                 }
+
+                dd("end");
 
                 Log::info(json_encode($filteredRawData));
                 return response()->json($filteredData);
             } else {
-                Log::error('API Request failed with status ' . $response->status());
+                Log::error('API Request failed with status # ' . $response->status());
                 return response()->json(['error' => 'API request failed'], $response->status());
             }
         } catch (\Exception $e) {
