360f76a8c88b63f73beb83165971f709328d249a
Ashan Kolambage <ashankolambage@gmail.com>
2024-06-02 16:40:28 +0530

    done done

--- app/app/Http/Controllers/ApiController.php
+++ app/app/Http/Controllers/ApiController.php
@@ -29,7 +29,6 @@ class ApiController extends Controller
                     'cmd' => 'system_status'
                 ]);
             }
-            
 
             if ($response->successful()) {
                 if ($routerType == "huawei") {
@@ -46,13 +45,6 @@ class ApiController extends Controller
                         'raw_modem_rsrq' => $data['data']['main_info']['modem_rsrq'] ?? 0,
                         'raw_modem_sinr' => $data['data']['main_info']['modem_sinr'] ?? 0,
                     ];
-    
-                    $filteredRawData = [
-                        'modem_rsrp' => $data['data']['main_info']['modem_rsrp'] ?? 0,
-                        'modem_rssi' => $data['data']['main_info']['modem_rssi'] ?? 0,
-                        'modem_rsrq' => $data['data']['main_info']['modem_rsrq'] ?? 0,
-                        'modem_sinr' => $data['data']['main_info']['modem_sinr'] ?? 0,
-                    ];
                 } elseif ($routerType == "outdoor") {
                     $data = json_decode($response, true);
 
@@ -67,16 +59,8 @@ class ApiController extends Controller
                         'raw_modem_rsrq' => $data['rsrq'] ?? 0,
                         'raw_modem_sinr' => $data['sinr'] ?? 0,
                     ];
-    
-                    $filteredRawData = [
-                        'modem_rsrp' => $data['rsrp'] ?? 0,
-                        'modem_rssi' => $data['rssi'] ?? 0,
-                        'modem_rsrq' => $data['rsrq'] ?? 0,
-                        'modem_sinr' => $data['sinr'] ?? 0,
-                    ];
                 }
 
-                Log::info(json_encode($filteredRawData));
                 return response()->json($filteredData);
             } else {
                 Log::error('API Request failed with status # ' . $response->status());
